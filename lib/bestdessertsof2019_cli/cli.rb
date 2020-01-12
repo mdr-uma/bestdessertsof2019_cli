e10a888fa492b7ad3a78f974761c3e48a1dac860
Uma Manandhar <um_2003_8@hotmail.com>
2020-01-12 22:47:13 +0000

    changed case statement to if/else statement

--- lib/bestdessertsof2019_cli/cli.rb
+++ lib/bestdessertsof2019_cli/cli.rb
@@ -4,12 +4,13 @@ class Bestdessertsof2019Cli::CLI
     puts "Welcome to Best Desserts of 2019!" 
     list_of_desserts
     options
-    closing
+    # closing
   end
   
   def list_of_desserts
     puts "Here are the Lists!"
-    puts <<-Doc
+    puts <<-Doc.gsub /^\s*/, ''
+    
     1.brownie
     2.apple pie
     3.blueberry cookie
@@ -19,28 +20,30 @@ class Bestdessertsof2019Cli::CLI
   end
   
   def options
+
     input = nil
     while input != "exit"
       puts "If you want any more info about particular dish, enter number of that dish or type list to see the all list again or type exit:"
     input = gets.strip
 
-      case input
-      when "1"
+      if input == "1"
         puts "more info on 1.."
-      when "2"
+      elsif input == "2"
         puts "more info on 2.."
-      when "3"
+      elsif input == "3"
         puts "more info on 3.."
-      when "4"
+      elsif input == "4"
         puts "more info on 4.."
-      when "5"
-      puts "more info on 5.."
-      when "list"
+      elsif input == "5"
+        puts "more info on 5.."
+      elsif input == "list"
         list_of_desserts
-    else 
-      puts "That number does not exit, please enter right number or type exit."
+      elsif input == "exit"
+        puts "Thank you for visiting. See you soon..."
+      else 
+        puts "That number does not exit, please enter right number or type exit."
+      end
     end
-  end
   end
     
     def closing
