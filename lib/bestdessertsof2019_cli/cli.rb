c2315f57ddfb3342d5f299c81668706cdb82a351
Uma Manandhar <um_2003_8@hotmail.com>
2020-01-12 18:09:45 +0000

    created methods inside cli.rb

--- lib/bestdessertsof2019_cli/cli.rb
+++ lib/bestdessertsof2019_cli/cli.rb
@@ -1,7 +1,39 @@
 class Bestdessertsof2019Cli::CLI
   
   def call
-    # puts "Here is the best desserts of 2019"       ..this is working 
+    puts "Welcome to Best Desserts of 2019!" 
+    list_of_desserts
+    options
   end
+  
+  def list_of_desserts
+    puts "Here are the Lists! 
+    1.
+    2.
+    3.
+    4.
+    5.
+    "
+  end
+  
+  def options
+    puts "If you want any more info about particular dish, enter number of that dish or type exit."
+      input = nil
+    while input != exit
+      input = gets.strips
+      case input
+      when "1"
+      puts "more info on 1.."
+      when "2"
+      puts "more info on 2.."
+    else 
+      puts "That number does not exit, please enter right number or type exit."
+    end
+    end
+  end
+    
+    def closing
+    puts "Thank you for visiting. See you soon..."
+    end
   
 end
