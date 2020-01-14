3d7d774738a4dfd552cd66bee7e34ddcd09138da
Uma Manandhar <um_2003_8@hotmail.com>
2020-01-14 04:49:25 +0000

    modified list_of_dessert, option_2 method and self.scrape_purewow

--- lib/bestdessertsof2019_cli/cli.rb
+++ lib/bestdessertsof2019_cli/cli.rb
@@ -12,64 +12,67 @@ class Bestdessertsof2019Cli::CLI
     puts "Take a quick peek at some tasty desserts?"
     puts "Enter: yes / no"
     input = nil 
-    # while input != "exit"
+     while input != "exit"
       input = gets.strip.downcase
+      
       if input == "yes"
         list_of_desserts
       elsif input == "no" || "exit"
       # binding.pry
-         input = "exit"
+        input = "exit"
         closing
       else
         puts "Invalid entry. Please enter yes or no"
       end 
-    # end
+     end
   end 
   
   
   def list_of_desserts
-    # puts "Here are the Lists!"
-    # puts <<-Doc.gsub /^\s*/, ''
-    
-    # 1.brownie
-    # 2.apple pie
-    # 3.blueberry cookie
-    # 4.rosted caramel puff
-    # 5.french roll
-    # Doc
-    
     @desserts = Bestdessertsof2019Cli::Dessert.list
-    option_2
+    @desserts.each do |list|
+      # binding.pry
+      # puts "#{list.name}"
+    end
+    # option_2
   end
   
-  def option_2
+  # def option_2
+  #   input = nil
+  #   while input != "exit"
+  #     puts "If you want any more info about particular dish, enter number of that dish or type list to see the all list again or type exit:"
+  #       input = gets.strip
+        
+  #       if input.to_i > 0
+  #         puts @desserts[input.to_i - 1]
+  #       elsif input == "list"
+  #       list_of_desserts
+  #       else 
+  #       puts "That number does not exist, please enter a right number."
+  #     end
+  #     end
+  #   end
 
-    input = nil
-    while input != "exit"
-      puts "If you want any more info about particular dish, enter number of that dish or type list to see the all list again or type exit:"
-    input = gets.strip
-
-      if input == "1"
-        puts "more info on 1.."
-      elsif input == "2"
-        puts "more info on 2.."
-      elsif input == "3"
-        puts "more info on 3.."
-      elsif input == "4"
-        puts "more info on 4.."
-      elsif input == "5"
-        puts "more info on 5.."
-      elsif input == "list"
-        list_of_desserts
-      elsif input == "exit"
-        # input = "exit"
-        closing
-        # puts "Thank you for visiting. See you soon..."
-      else 
-        puts "That number does not exist, please enter right a number."
-      end
-    end
-  end
+      # if input == "1"
+      #   puts "more info on 1.."
+      # elsif input == "2"
+      #   puts "more info on 2.."
+      # elsif input == "3"
+      #   puts "more info on 3.."
+      # elsif input == "4"
+      #   puts "more info on 4.."
+      # elsif input == "5"
+      #   puts "more info on 5.."
+      # elsif input == "list"
+      #   list_of_desserts
+      # elsif input == "exit"
+      #   # input = "exit"
+      #   closing
+      #   # puts "Thank you for visiting. See you soon..."
+      # else 
+      #   puts "That number does not exist, please enter right a number."
+      # end
+    
     
     def closing
       puts "Thank you for visiting. See you soon..."
--- lib/bestdessertsof2019_cli/desserts.rb
+++ lib/bestdessertsof2019_cli/desserts.rb
@@ -5,14 +5,14 @@ class Bestdessertsof2019Cli::Dessert
   
   def self.list
     puts "Here are the Lists!"
-    puts <<-Doc.gsub /^\s*/, ''
+    # puts <<-Doc.gsub /^\s*/, ''
     
-    1.brownie
-    2.apple pie
-    3.blueberry cookie
-    4.rosted caramel puff
-    5.french roll
-    Doc
+    # 1.brownie
+    # 2.apple pie
+    # 3.blueberry cookie
+    # 4.rosted caramel puff
+    # 5.french roll
+    # Doc
     
     self.scrape_lists
   end
@@ -26,7 +26,11 @@ class Bestdessertsof2019Cli::Dessert
   end
   
   def self.scrape_purewow
-     doc = Nokogiri::HTML(open("https://www.purewow.com/food/best-desserts-2019"))
+    # doc = Nokogiri::HTML(open("https://www.purewow.com/food/best-desserts-2019"))
+     doc = Nokogiri::HTML(open("https://www.cnn.com/travel/article/world-50-best-desserts/index.html"))
+     list = self.new
+    # list.name = doc.css("h2.firsth2").text.strip
+    list.name = doc.css("span h3").text
     
     # binding.pry
   end
