5f599c1802d7f2555de2798a6807fa3d4ad47571
Uma Manandhar <um_2003_8@hotmail.com>
2020-01-16 21:32:58 +0000

    tiny change to test branch

--- lib/bestdessertsof2019_cli/desserts.rb
+++ lib/bestdessertsof2019_cli/desserts.rb
@@ -20,7 +20,6 @@ class Bestdessertsof2019Cli::Dessert
   
   def self.scrape_purewow
     doc = Nokogiri::HTML(open("https://www.purewow.com/food/best-desserts-2019"))
-      
     dessert = self.new
     dessert.name = doc.css(".description").collect {|heading| heading.css("h2").text}
     dessert.description = doc.css(".description").collect {|paragraph| paragraph.css("p").text.gsub("Get the recipe", "")}
